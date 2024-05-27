5e64b3e0a1528df7eef6f77f39af8975b3eb4da4
Hinata0607 <atahin0607@gmail.com>
2024-05-28 05:58:08 +0900

    :fix header title

--- src/layouts/atom/HeaderTitle.tsx
+++ src/layouts/atom/HeaderTitle.tsx
@@ -4,6 +4,10 @@ export const HeaderTitle = () => {
 	return (
 		<h2
 			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				width: '250px',
 				fontWeight: 'bold',
 			}}
 		>
