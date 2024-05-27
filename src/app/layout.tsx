67da788ec856ff53ed8f281d16be28d7d031479d
Hinata0607 <atahin0607@gmail.com>
2024-05-28 04:25:54 +0900

    :add first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "RESAS Graph",
+  description: "RESAS APIを利用した都道府県別の総人口推移グラフのアプリケーション",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="jp">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
