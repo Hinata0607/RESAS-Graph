ba7dcfb606379e068a42dd9b1aa23871f1cfa68c
Hinata0607 <atahin0607@gmail.com>
2024-05-29 17:58:43 +0900

    :update header responsive

--- src/layouts/atom/HeaderTitle.tsx
+++ src/layouts/atom/HeaderTitle.tsx
@@ -1,13 +1,16 @@
+import { useBreakpoint } from '@/hooks';
 import React from 'react';
 
 export const HeaderTitle = () => {
+	const breakpoint = useBreakpoint();
+	const isSm = ['xs', 'sm'].includes(breakpoint);
 	return (
 		<h2
 			style={{
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
-				width: '250px',
+				width: isSm ? 'fit-content' : '250px',
 				fontWeight: 'bold',
 			}}
 		>
