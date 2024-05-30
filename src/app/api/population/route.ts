7fdc852f2a6294da7c32664f341e39eda2e85de1
Hinata0607 <atahin0607@gmail.com>
2024-05-31 02:00:34 +0900

    :fix env key name changed

--- src/app/api/population/route.ts
+++ src/app/api/population/route.ts
@@ -5,10 +5,10 @@ import { NextRequest, NextResponse } from 'next/server';
 export const GET = async (req: NextRequest) => {
 	const { searchParams } = new URL(req.url);
 	const prefCode = searchParams.get('prefCode');
-	const url = `${process.env.NEXT_PUBLIC_RESAS_BASE_URL}/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`;
+	const url = `${process.env.RESAS_BASE_URL}/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`;
 	const response = NextResponse.json(
 		await axiosFetch.get<PrefectureResponse>(url, {
-			'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY || '',
+			'X-API-KEY': process.env.RESAS_API_KEY || '',
 		})
 	);
 	return response;
